b2e3c924b4a6a22c9a3202643d4290ce3fad73b1
Actav <actav@mail.com>
2023-01-31 01:31:18 +0300

    FIX

--- task_01/main.go
+++ task_01/main.go
@@ -7,33 +7,33 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+  "fmt"
+  "sync"
 )
 
 var (
-	wg sync.WaitGroup
-	m sync.Mutex
-	sem = make(chan struct{}, 50)
+  wg sync.WaitGroup
+  m sync.Mutex
+  sem = make(chan struct{}, 50)
 )
 
 func main() {
-	counter := 0
+  counter := 0
 
-	for i := 0; i < 1000; i++ {
-		wg.Add(1)
-		sem <- struct{}{}
+  for i := 0; i < 1000; i++ {
+    wg.Add(1)
+    sem <- struct{}{}
 
-		go func() {
-			m.Lock()
-				counter++
-			m.Unlock()
+    go func() {
+      m.Lock()
+        counter++
+      m.Unlock()
 
-			<-sem
-			wg.Done()
-		}()
-	}
+      <-sem
+      wg.Done()
+    }()
+  }
 
-	wg.Wait()
-	fmt.Println("Counter:", counter)
+  wg.Wait()
+  fmt.Println("Counter:", counter)
 }
